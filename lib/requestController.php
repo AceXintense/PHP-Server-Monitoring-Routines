b91e3a658d96f54ad5cda1945f6be90560ccaf40
Sam Grew <shanker1210@hotmail.co.uk>
2017-02-22 20:31:24 +0000

    Changed the curl service to use try catch statement.

--- lib/requestController.php
+++ lib/requestController.php
@@ -47,23 +47,27 @@ class requestController {
     public function post($arguments, $location) {
         //next example will insert new conversation
         $service_url = $this->serverURL . $location;
-        $curl = curl_init($service_url);
-        $curl_post_data = $arguments;
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-        $curl_response = curl_exec($curl);
-        if ($curl_response === false) {
-            $info = curl_getinfo($curl);
+        try {
+            $curl = curl_init($service_url);
+            $curl_post_data = $arguments;
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+            $curl_response = curl_exec($curl);
+            if ($curl_response === false) {
+                $info = curl_getinfo($curl);
+                curl_close($curl);
+                die('error occured during curl exec. Additional info: ' . var_export($info));
+            }
             curl_close($curl);
-            die('error occured during curl exec. Additional info: ' . var_export($info));
+            $decoded = json_decode($curl_response);
+            if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+                die('error occured: ' . $decoded->response->errormessage);
+            }
+            echo 'response ok!';
+        } catch (Exception $exception) {
+            die($exception);
         }
-        curl_close($curl);
-        $decoded = json_decode($curl_response);
-        if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-            die('error occured: ' . $decoded->response->errormessage);
-        }
-        echo 'response ok!';
     }
 
     public function get($location) {
